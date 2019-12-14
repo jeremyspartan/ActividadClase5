38b50792dd842b9f527d9fe2139c8b08ed57c6e5
jeremyspartan <jyvebuz@gamil.com>
2019-12-13 20:35:10 -0600

    Fix

--- Actividad2/LoginForm.Designer.cs
+++ Actividad2/LoginForm.Designer.cs
@@ -115,7 +115,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.CancelarButton;
-            this.ClientSize = new System.Drawing.Size(509, 253);
+            this.ClientSize = new System.Drawing.Size(492, 253);
             this.Controls.Add(this.aceptarButton);
             this.Controls.Add(this.CancelarButton);
             this.Controls.Add(this.contraseñaLabel);
