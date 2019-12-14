001c2ee5d57ea947b79aa8fb2e1f5f0b120e883b
thonyva16 <48537334+thonyva16@users.noreply.github.com>
2019-12-14 09:13:58 -0600

    Validación de información del estudiante.

--- Actividad2/UNA/Dashboard/Objetos/Usuario.cs
+++ Actividad2/UNA/Dashboard/Objetos/Usuario.cs
@@ -8,5 +8,44 @@ namespace Actividad2.UNA.Dashboard.Objetos
 {
     class Usuario
     {
+        public string Nombre
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public string Apellidos
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public DateTime FechaNacimiento
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public string Direccion
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int Observaciones
+        {
+            get => default;
+            set
+            {
+            }
+        }
     }
 }
